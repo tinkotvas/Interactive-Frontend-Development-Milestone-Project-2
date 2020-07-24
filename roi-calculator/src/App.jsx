36cc480ffbe109faffe93a6a7acc7a5e2d631228
Robin Westerback <robinwesterback1989@gmail.com>
2020-07-24 12:49:39 +0000

    Create component SiteHeader

---
+++ roi-calculator/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import SiteHeader from './components/SiteHeader';
+
+function App() {
+  return (
+      <div className="body">
+        <SiteHeader logoName="ROI Calculator" />
+      </div>
+    );
+}
+
+export default App;
